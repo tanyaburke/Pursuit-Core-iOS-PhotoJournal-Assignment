def3d6f4cb0fccd0f6b76d8aa4c3beeac2f95d8e
tanyaburke <tanyaburke@pursuit.org>
2020-02-18 21:12:30 -0500

    Adding UI

---
+++ PhotoJournalApp/PhotoJournalApp/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoJournalApp
+//
+//  Created by Tanya Burke on 2/18/20.
+//  Copyright © 2020 Tanya Burke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
